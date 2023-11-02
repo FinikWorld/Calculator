7a1cd533b5635d6b05a322b5d0a558b88d40064b
Matvey Alesin <finikworld@gmail.com>
2023-11-02 12:25:25 +0300

    First commit

--- m11/Constants.swift
+++ m11/Constants.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+enum ConstantStrings {
+    enum Calculator {
+        static let title = "Calculator"
+    }
+}
+
+enum MathOperation {
+    case sum, substract, divide, multiply
+    
+    func makeOperation(result: Int, newValue: Int) -> Int {
+        switch self {
+        case .sum:
+            return (result + newValue)
+        case .substract:
+            return (result - newValue)
+        case .divide:
+            if newValue != 0 {
+                return (result / newValue)
+            } else {return 0}
+        case .multiply:
+            return (result * newValue)
+        }
+        
+    }
+}
